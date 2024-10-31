990e82ac486333d58774df37ec168ddabdf220f5
ваша_почта <_@example.com>
2024-10-31 16:12:29 +0300

    Калькус

--- NodeConsoleApp1/app.js
+++ NodeConsoleApp1/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Hell worlds');
+
+// Простейший калькулятор
+function calculate(operation, num1, num2) {
+    switch(operation) {
+        case 'add':
+            return num1 + num2;
+        case 'subtract':
+            return num1 – num2;
+        case 'multiply':
+            return num1 * num2;
+        case 'divide':
+            return num1 / num2;
+        default:
+            return 'Invalid operation';
+    }
+}
